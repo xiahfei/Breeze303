7f85f15bf6f91c6c3966b85f9b0a13a02896c273
xiahfei <124034214+xiahfei@users.noreply.github.com>
2025-05-22 10:04:31 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -67,7 +67,7 @@ git clone --depth=1 https://github.com/ilxp/luci-app-ikoolproxy package/luci-app
 git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
 git clone --depth=1 https://github.com/destan19/OpenAppFilter package/OpenAppFilter
 git clone --depth=1 https://github.com/Jason6111/luci-app-netdata package/luci-app-netdata
-git clone --depth=1 -b main https://github.com/lwb1978/openwrt-gecoosac
+git clone --depth=1 https://github.com/lwb1978/openwrt-gecoosac
 git clone --depth=1 https://github.com/linkease/nas-packages-luci/tree/main/luci/luci-app-linkease
 git_sparse_clone main https://github.com/Lienol/openwrt-package luci-app-filebrowser luci-app-ssr-mudb-server
 git_sparse_clone openwrt-18.06 https://github.com/immortalwrt/luci applications/luci-app-eqos
